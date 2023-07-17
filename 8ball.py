f287b256b28db6df0e05b8fdd3455263ba2dd717
Richard Pilipenko <richardpilipenko@Jennys-MacBook-Pro.local>
2023-07-18 00:48:00 +0100

    defo implemented features

--- 8ball.py
+++ 8ball.py
@@ -1,11 +1,28 @@
 import random
 
+eight_ball_answers = {'Affirmative': ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 
+                      'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.'], 
+                      'Non-commital': ['Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 
+                      'Concentrate and ask again.'], 'Negative': ['Don\'t count on it.', 'My reply is no.', 'My sources say no.', 
+                      'Outlook not so good.', 'Very doubtful.']}
 
-def get_eightball_answer(user_name, user_question):
-    pass
+answer_key = ['Affirmative', 'Non-commital', 'Negative']
+
+def get_eightball_answer(key):
+    answer = get_random_choice(key)
+    reply_choice = get_reply(eight_ball_answers[answer])
+    print(answer)
+    print(eight_ball_answers[answer][reply_choice])
+    
+    
 
 
-def get_random_choice():
-    pass
+def get_random_choice(key):
+    return random.choice(key)
 
+def get_reply(list):
+    return random.randrange(0, len(list))
 
+print('Ask')
+usr_input = input('')
+get_eightball_answer(answer_key)
