cb00d71dd9e87fef377bc1af9fd605148a17a69f
Richard Pilipenko <richardpilipenko@Jennys-MacBook-Pro.local>
2023-07-19 22:17:10 +0100

    base logic

--- 8ball.py
+++ 8ball.py
@@ -1,28 +1,28 @@
 import random
+import time
+import sys
 
-eight_ball_answers = {'Affirmative': ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 
-                      'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.'], 
-                      'Non-commital': ['Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 
-                      'Concentrate and ask again.'], 'Negative': ['Don\'t count on it.', 'My reply is no.', 'My sources say no.', 
-                      'Outlook not so good.', 'Very doubtful.']}
+eight_ball_answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definitely.", "You may rely on it.", 
+                      "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", 
+                      "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", 
+                      "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", 
+                      "Outlook not so good.", "Very doubtful."]
 
-answer_key = ['Affirmative', 'Non-commital', 'Negative']
+play = True
 
-def get_eightball_answer(key):
-    answer = get_random_choice(key)
-    reply_choice = get_reply(eight_ball_answers[answer])
-    print(answer)
-    print(eight_ball_answers[answer][reply_choice])
-    
-    
+def magic_eight_ball():
+    response_question = input("You can ask me any yes or no question:\n>>>")
+    print('Thinking...')
+    time.sleep(random.randint(0, 4))
+    print(random.choice(eight_ball_answers))
 
-
-def get_random_choice(key):
-    return random.choice(key)
-
-def get_reply(list):
-    return random.randrange(0, len(list))
-
-print('Ask')
-usr_input = input('')
-get_eightball_answer(answer_key)
+while play:
+    response_name = input("Hello! I am Magic 8 Ball. What is your name?\n>>>")
+    time.sleep(random.randint(1, 2))
+    print(f"Nice to meet you {response_name}!")
+    time.sleep(random.randint(1, 2))
+    magic_eight_ball()
+    ask_again = input("Would you like to ask again?[Y/N]")
+    if not ask_again.lower() == 'y':
+        play = 'n'
+        sys.exit()
