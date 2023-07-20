b7727370fedf061d19b550a893992add9ae1cdc9
Richard Pilipenko <richardpilipenko@Jennys-MacBook-Pro.local>
2023-07-20 01:29:59 +0100

    Improved Functionality

--- 8ball.py
+++ 8ball.py
@@ -8,23 +8,25 @@ eight_ball_answers = ["It is certain.", "It is decidedly so.", "Without a doubt.
                       "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", 
                       "Outlook not so good.", "Very doubtful."]
 
-play = True
+user_name = ""
+first_question = True
 
 def magic_eight_ball():
-    response_question = input("You can ask me any yes or no question:\n>>>")
-    print('Thinking...')
-    time.sleep(random.randint(0, 4))
+    global first_question
+    if first_question == True: 
+        response_question = input("You can ask me any yes or no question.\n>>>")
+        first_question = False
+    else:
+        response_new_question = input("Fire away.\n>>>")
+    print("Fortune telling in progress...")
+    time.sleep(random.randint(1, 4))
     print(random.choice(eight_ball_answers))
 
-while play:
-    response_name = input("Hello! I am Magic 8 Ball. What is your name?\n>>>")
-    time.sleep(random.randint(1, 2))
-    print(f"Nice to meet you {response_name}!")
-    time.sleep(random.randint(1, 2))
+     
+while True:
+    if len(user_name) == 0: user_name = input("What is your name?\n>>>")
     magic_eight_ball()
-    ask_again = input("Would you like to ask again?[Y/N]")
-    if not ask_again.lower() == 'y':
-        play = 'n'
+    ask_again = input("Do you have another question?[Y/N]\n>>>")
+    if not ask_again.lower() == "y":
+        print("Bye!")
         sys.exit()
-
-dsada
